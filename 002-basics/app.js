b3a8e05008b998ce38f3b6f7c46c5f5a73dbe6de
Filip Blajiu <filipblajiu@gmail.com>
2020-12-01 00:00:45 -0800

    Using methods

--- 002-basics/app.js
+++ 002-basics/app.js
@@ -1,10 +1,21 @@
 const app = Vue.createApp({
     data() {
-        return {
-            courseGoal: 'Finish the course and learn Vuee!',
-            vueLink: 'https://vuejs.org/'
-         };
-    }
-});
-
-app.mount('#user-goal');
+      return {
+        courseGoal: "Finish the course and learn Vuee!",
+        vueLink: "https://vuejs.org/",
+      };
+    },
+    methods: {
+      outputGoal() {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5) {
+          return "Learn Vue!";
+        } else {
+          return "Master Vue!";
+        } 
+      },
+    },
+  });
+  
+  app.mount("#user-goal");
+  
