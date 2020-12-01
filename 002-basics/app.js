b569ecc1e9030eb1c82d01c0ab5d764fbfa83fce
Filip Blajiu <filipblajiu@gmail.com>
2020-12-01 11:07:04 +0200

    Output HTML using v-html

--- 002-basics/app.js
+++ 002-basics/app.js
@@ -1,22 +1,21 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        courseGoalA: "Finish the course and learn Vuee!",
-        courseGoalB: "Master Vue and build amazing apps!",
-        vueLink: "https://vuejs.org/",
-      };
+  data() {
+    return {
+      courseGoalA: "Finish the course and learn Vuee!",
+      courseGoalB: "<h2>Master Vue and build amazing apps!</h2>",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
     },
-    methods: {
-      outputGoal() {
-        const randomNumber = Math.random();
-        if (randomNumber < 0.5) {
-          return this.courseGoalA;
-        } else {
-          return this.courseGoalB ;
-        } 
-      },
-    },
-  });
-  
-  app.mount("#user-goal");
-  
+  },
+});
+
+app.mount("#user-goal");
