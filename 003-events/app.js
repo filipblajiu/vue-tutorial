cb4b45985730fa1a210d0737c8bb89ee9d8c4714
Filip Blajiu <filipblajiu@gmail.com>
2020-12-01 11:57:29 +0200

    Use  argument

--- 003-events/app.js
+++ 003-events/app.js
@@ -12,8 +12,8 @@ const app = Vue.createApp({
     remove(num) {
       this.counter = this.counter - num;
     },
-    setName(event) {
-      this.name = event.target.value;
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
     }
   }
 });
